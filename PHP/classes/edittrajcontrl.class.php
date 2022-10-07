1f55c96c3ad1e19c1e607b0bfd578ba2985d3ebd
NTERAS <k.nteras@codeur.online>
2022-10-07 13:42:37 +0200

    edit for etapes, works fine

--- PHP/classes/edittrajcontrl.class.php
+++ PHP/classes/edittrajcontrl.class.php
@@ -4,27 +4,29 @@
 class EditatrajContrl extends Edittarj{
     public $idt;
     public $depart;
+    public $depart1;
+    public $depart2;
     public $arriver;
     public $calendar;
     public $routehours;
     public $routetype;
     public $guest;
-    public $intermediaire;
     
 
-    public function __construct($idt,$depart,$arriver,$calendar,$routehours,$routetype,$guest,$intermediaire){
+    public function __construct($idt,$depart,$depart1,$depart2,$arriver,$calendar,$routehours,$routetype,$guest){
         $this->idt = $idt;
+        $this->depart1 = $depart1;
+        $this->depart2 = $depart2;
         $this->depart = $depart;
         $this->arriver = $arriver;
         $this->calendar = $calendar;
         $this->routehours = $routehours;
         $this->routetype = $routetype;
         $this->guest = $guest;
-        $this->intermediaire = $intermediaire;
     }
 
     public function MyEdittraj(){
-        $this->Edit($this->idt,$this->depart,$this->arriver,$this->calendar,$this->routehours,$this->routetype,$this->guest,$this->intermediaire);
+        $this->Edit($this->idt,$this->depart,$this->depart1,$this->depart2,$this->arriver,$this->calendar,$this->routehours,$this->routetype,$this->guest);
     }
 }
 
--- PHP/includes/edittraj.inc.php
+++ PHP/includes/edittraj.inc.php
@@ -1,19 +1,21 @@
 <?php
 $idt = $_GET["id"];
 $depart = $_GET['location'];
+$depart1 = $_GET['locationAdd'];
+$depart2 = $_GET['locationAdd2'];
 $arriver = $_GET['arrivee'];
 $calendar = $_GET['date'];
 $routehours = $_GET['heure'];
 $routetype = $_GET['road'];
 $guest = $_GET['place'];
-$intermediaire = $_GET['locationAdd'];
+// $intermediaire = $_GET['locationAdd'];
 
 
 include('../classes/dbh.class.php');
 include('../classes/edittraj.class.php');
 include('../classes/edittrajcontrl.class.php');
 
-$traject = new EditatrajContrl($idt,$depart,$arriver,$calendar,$routehours,$routetype,$guest,$intermediaire);
+$traject = new EditatrajContrl($idt,$depart,$depart1,$depart2,$arriver,$calendar,$routehours,$routetype,$guest);
 $traject->MyEdittraj();
 
 
